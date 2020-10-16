42df68d5fb5a22971fbed6b63e99ee559b9f6c25
cathimn <catclee@gmail.com>
2020-10-15 22:30:09 -0500

    Basic layout

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  let name = document.getElementById("name");
+  let names = document.querySelectorAll(".name");
+  let cathimn = document.querySelectorAll(".cathimn")
+  let notCathimn = document.querySelectorAll(".notcathimn");
 
-  function changeName() {
-    
-  }
+  let links = document.querySelectorAll("a");
+  links.forEach(link => link.classList.add("light-mode"));
 
-  changeName();
+  names.forEach(name => name.addEventListener("mouseenter", () => {
+    notCathimn.forEach(not => not.classList.add("hidden"));
+  }))
+  
+  names.forEach(name => name.addEventListener("mouseleave", () => {
+    notCathimn.forEach(not => not.classList.remove("hidden"));
+  }))
+
+  document.getElementById("mode").addEventListener("click", () => {
+    document.getElementById("mode").classList.toggle("light-mode");
+    document.getElementById("mode").classList.toggle("dark-mode");
+    document.querySelector("body").classList.toggle("light-mode")
+    document.querySelector("body").classList.toggle("dark-mode")
+  })
 })
